ff1b1b761468a2f1ff909ca420bde5b8c8917164
Your Name <you@example.com>
2026-02-10 11:53:22 -0500

    meh

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,10 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+    /*Icon*/    /*Command*/                                                     /*Update Interval*/  /*Update Signal*/
+    {"Mem:",    "awk '/MemTotal/{t=$2*1024} /MemAvailable/{a=$2*1024} END{u=t-a; used_val=(u>=1e9?u/1e9:u/1e6); unit=(u>=1e9?\"G\":\"M\"); printf \"%.1f%s / %dGB\\n\", used_val, unit, int(t/1e9)}' /proc/meminfo",             25,                  0},
 
-	{"", "date '+%B %d, %I:%M%p'",					5,		0},
+
+    {"",        "date '+%B %d, %I:%M%p'",                                       60,                  0},
 };
 
-//sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char delim[] = " - ";
 static unsigned int delimLen = 5;
